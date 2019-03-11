236739ced0f5815d0e3e9c08b434f3687df8c88d
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 10:04:47 -0700

    updated ft_strsub.c

--- ft_strsub.c
+++ ft_strsub.c
@@ -3,40 +3,38 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strsub.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angkim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 17:31:18 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/04 12:18:24 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/11 10:03:22 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** takes a string, a start index, and a substring length as args
-** returns substring in a 'fresh' string (malloc)
-** if allocation fails, return NULL
+** Allocates (with malloc(3)) and returns a “fresh” substring from the string
+** given as argument. The substring begins at indexstart and is of size len. 
+** If start and len aren’t refering to a valid substring, the behavior is
+** undefined. If the allocation fails, the function returns NULL.
 */
 
 char	*ft_strsub(char const *s, unsigned int start, size_t len)
 {
-	char	*sub_str;
-	size_t	i;
+	char			*sub_str;
+	unsigned int	i;
 
-	sub_str = (char *)malloc(sizeof(*sub_str) * len + 1);
-	i = 0;
+	sub_str = (char *)ft_memalloc(len + 1);
 	if (s && sub_str)
 	{
 		i = 0;
-		while (i < len)
+		while (len--)
 		{
-			sub_str[i] = s[start];
+			sub_str[i] = s[start + i];
 			i++;
-			start++;
 		}
 		sub_str[i] = '\0';
 		return (sub_str);
 	}
-	else
-		return (NULL);
+	return (NULL);
 }
