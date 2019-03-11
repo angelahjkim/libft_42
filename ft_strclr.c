1a809a27691624b02135c4a002a5cfb3a864f799
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 09:43:42 -0700

    updated ft_strclr.c

--- ft_strclr.c
+++ ft_strclr.c
@@ -3,30 +3,24 @@
 /*                                                        :::      ::::::::   */
 /*   ft_strclr.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angkim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 14:51:55 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/04 11:33:45 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/11 09:43:09 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** takes a str as param and sets value of each character to '\0'
+** Sets every character of the string to the value ’\0’.
 */
 
 void	ft_strclr(char *s)
 {
-	size_t len;
-
-	if (s)
+	if (s && *s)
 	{
-		len = ft_strlen(s);
-		while (len--)
-		{
-			*s = '\0';
-			s++;
-		}
+		*s = '\0';
+		s++;
 	}
 }
