b43a83fade7c9bd8e0d70adfe1702139ebce44c0
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 09:20:51 -0700

    fixed ft_strncmp.c

--- ft_strncmp.c
+++ ft_strncmp.c
@@ -6,7 +6,7 @@
 /*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/21 21:25:28 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/11 08:44:51 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/11 09:20:11 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -26,7 +26,7 @@ int		ft_strncmp(const char *s1, const char *s2, size_t n)
 	i = 0;
 	if (!s1 || !s2 || !n)
 		return (0);
-	while (s1[i] == s2[i] && s1[i])
+	while ((s1[i] == s2[i]) && s1[i] && (i < n - 1)
 		i++;
 	return ((unsigned char)s1[i] - (unsigned char)s2[i]);
 }
