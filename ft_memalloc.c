6b2b30227d8eea531333b94bc5169fbffcd733bb
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 09:32:22 -0700

    updated ft_memalloc.c ft_memdel.c

--- ft_memalloc.c
+++ ft_memalloc.c
@@ -3,10 +3,10 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memalloc.c                                      :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angkim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 20:25:25 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/08 14:57:34 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/11 09:29:02 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,23 +20,13 @@
 
 void	*ft_memalloc(size_t size)
 {
-	unsigned char	*mem_area;
-	size_t			i;
+	void *new;
 
-	if ((unsigned long long)(size - 1))
+	new = (void *)malloc(size);
+	if (new)
 	{
-		mem_area = (void *)malloc(sizeof(*mem_area) * size);
-		if (mem_area)
-		{
-			i = 0;
-			while (i < size)
-			{
-				mem_area[i] = 0;
-				i++;
-			}
-			if (i == size)
-				return ((void *)mem_area);
-		}
+		ft_bzero(new, size);
+		return (new);
 	}
-	return (0);
+	return (NULL);
 }
--- ft_memdel.c
+++ ft_memdel.c
@@ -3,25 +3,25 @@
 /*                                                        :::      ::::::::   */
 /*   ft_memdel.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: angkim <marvin@42.fr>                      +#+  +:+       +#+        */
+/*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/22 22:56:39 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/04 11:32:16 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/11 09:31:01 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** takes as a param the address of a mem area that needs to be freed with free
-** then puts the ptr to NULL
+** Takes as a parameter the address of a memory area that needs to be freed
+** with free, then puts the pointer to NULL.
 */
 
 void	ft_memdel(void **ap)
 {
-	if (ap)
+	if (ap && *ap)
 	{
 		free(*ap);
-		*ap = 0;
+		*ap = NULL;
 	}
 }
