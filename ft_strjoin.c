fbcf6832b7dfcfa71d1d1b66cf49751238f1b621
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 23:06:16 -0700

    updated ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,15 +1,35 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_strjoin.c                                       :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/02/23 21:42:36 by angkim            #+#    #+#             */
+/*   Updated: 2019/03/11 15:01:15 by angkim           ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
 #include "libft.h"
+
+/*
+** Allocates (with malloc(3)) and returns a “fresh” string ending with ’\0’,
+** result of the concatenation of s1 and s2. If the allocation fails the
+** function returns NULL.
+*/
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*new;
+	char	*cat_str;
+	size_t	total_len;
 
 	if (!s1 || !s2)
 		return (NULL);
-	new = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1);
-	if (!new)
+	total_len = ft_strlen(s1) + ft_strlen(s2);
+	cat_str = (char *)ft_memalloc(total_len + 1);
+	if (!cat_str)
 		return (NULL);
-	ft_strcpy(new, s1);
-	ft_strcat(new, s2);
-	return (new);
+	ft_strcpy(cat_str, s1);
+	ft_strcat(cat_str, s2);
+	return (cat_str);
 }
