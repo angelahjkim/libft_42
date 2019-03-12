9134b0af751832967e7cfb4a03d97ba394b36040
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 23:02:21 -0700

    test ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -4,8 +4,11 @@ char	*ft_strjoin(char const *s1, char const *s2)
 {
 	char	*new;
 
-	NULL_CHECK(!s1 || !s2);
-	NULL_CHECK(!(new = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1)));
+	if (!s1 || !s2);
+		return (NULL);
+	new = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1)));
+	if (!new)
+		return (NULL);
 	ft_strcpy(new, s1);
 	ft_strcat(new, s2);
 	return (new);
