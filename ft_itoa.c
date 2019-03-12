7958730b611a1b9dfe1b41ee13df83de6ea80b97
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-12 00:40:11 -0700

    updated ft_itoa.c

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 18:13:24 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/11 15:55:36 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/12 00:33:17 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,42 +18,25 @@
 ** supported. If the allocation fails, the function returns NULL.
 */
 
-static char	*ft_itoa_min(int n, int len)
-{
-	char	*ret;
-
-	ret = (char *)malloc(sizeof(char) * len + 1);
-	if (ret)
-	{
-		n = 2147483647;
-		ret = ft_nbrtostr(ret, n, len);
-		ret = ft_strrev(ret, len - 1);
-		ret[0] = '-';
-		ret[len - 1] = '8';
-		return (ret);
-	}
-	return (0);
-}
-
 char		*ft_itoa(int n)
 {
 	char	*i_str;
+	long	num;
 	int		len;
 	int		neg;
 
-	len = ft_digitcount(n);
-	if (n == -2147483648)
-		return (ft_itoa_min(n, len));
+	num = n;
+	len = ft_digitcount(num);
 	i_str = (char *)ft_memalloc(len + 1);
 	if (!i_str)
 		return (NULL);
 	neg = 0;
 	if (n < 0)
 	{
-		n *= -1;
+		num *= -1;
 		neg = 1;
 	}
-	i_str = ft_nbrtostr(i_str, n, len);
+	i_str = ft_nbrtostr(i_str, num, len);
 	i_str = ft_strrev(i_str, len - 1);
 	if (neg)
 		i_str[0] = '-';
