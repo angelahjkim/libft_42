35892cc00afc00b575bfc0dc0ecb9e88b632a160
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 23:17:45 -0700

    updates

--- ft_strnequ.c
+++ ft_strnequ.c
@@ -6,7 +6,7 @@
 /*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/23 17:03:35 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/11 09:56:42 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/11 23:17:21 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,7 +19,7 @@
 
 int		ft_strnequ(char const *s1, char const *s2, size_t n)
 {
-	if (s1 && s2)
+	if (s1 && s2 && n)
 	{
 		if (!ft_strncmp(s1, s2, n))
 			return (1);
