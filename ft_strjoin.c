0aebc2a2a8a3ae0334e40e9a019c25db8b77c240
Angela Kim <angkim@e1z2r8p12.42.us.org>
2019-03-11 22:59:03 -0700

    test ft_strjoin.c

--- ft_strjoin.c
+++ ft_strjoin.c
@@ -1,35 +1,12 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   ft_strjoin.c                                       :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/02/23 21:42:36 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/11 14:55:55 by angkim           ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
 #include "libft.h"
-
-/*
-** Allocates (with malloc(3)) and returns a “fresh” string ending with ’\0’,
-** result of the concatenation of s1 and s2. If the allocation fails the
-** function returns NULL.
-*/
 
 char	*ft_strjoin(char const *s1, char const *s2)
 {
-	char	*cat_str;
-	size_t	total_len;
+	char	*new;
 
-	if (!s1 || !s2)
-		return (NULL);
-	total_len = ft_strlen(s1) + ft_strlen(s2);
-	cat_str = (char *)ft_memalloc(total_len + 1);
-	if (!cat_str)
-		return (NULL);
-	ft_strcpy(cat_str, s1);
-	ft_strcat(cat_str, s2);
-	return (cat_str);
+	NULL_CHECK(!s1 || !s2);
+	NULL_CHECK(!(new = (char*)ft_memalloc(ft_strlen(s1) + ft_strlen(s2) + 1)));
+	ft_strcpy(new, s1);
+	ft_strcat(new, s2);
+	return (new);
 }
