a5348aa8fbf13646058442cbf13d4a284d45dd1f
Angela Kim <angkim@e1z2r8p8.42.us.org>
2019-03-16 10:00:20 -0700

    ft_itoa.c int min change

--- ft_itoa.c
+++ ft_itoa.c
@@ -6,7 +6,7 @@
 /*   By: angkim <angkim@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/02/24 18:13:24 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/16 07:58:01 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/16 09:57:33 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,16 +18,55 @@
 ** supported. If the allocation fails, the function returns NULL.
 */
 
+static char	*ft_inttostr(char *s, int n, int len)
+{
+	int i;
+
+	i = 0;
+	if (n == 0)
+	{
+		s[0] = '0';
+		s[1] = '\0';
+		return (s);
+	}
+	while (i < len)
+	{
+		if (n % 10 == 0 && n != 0)
+			s[i] = '0';
+		if (n % 10)
+			s[i] = n % 10 + '0';
+		n /= 10;
+		i++;
+	}
+	return (s);
+}
+
+static char	*ft_strrev(char *s, int len)
+{
+	int		i;
+	char	tmp;
+
+	i = 0;
+	while (i < len / 2)
+	{
+		tmp = s[i];
+		s[i] = s[len - 1 - i];
+		s[len - 1 - i] = tmp;
+		i++;
+	}
+	return (s);
+}
+
 char		*ft_itoa(int n)
 {
 	char	*i_str;
-	long	num;
+	int		num;
 	int		len;
 	int		neg;
 
+	if (n == -2147483648)
+		return (ft_strdup("-2147483648"));
 	num = n;
-	if (num < -2147483648 || n > 2147483647)
-		return (NULL);
 	len = ft_digitcount(num);
 	if (!(i_str = (char *)ft_memalloc(len + 1)))
 		return (NULL);
@@ -37,7 +76,7 @@ char		*ft_itoa(int n)
 		num *= -1;
 		neg = 1;
 	}
-	ft_nbrtostr(i_str, num, len);
+	ft_inttostr(i_str, num, len);
 	ft_strrev(i_str, len);
 	if (neg)
 		i_str[0] = '-';
