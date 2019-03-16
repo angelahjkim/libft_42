c503c58b89418a72bf88b7e2b79adb89571a2903
Angela Kim <angkim@e1z2r8p8.42.us.org>
2019-03-16 10:28:25 -0700

    final push

--- ft_print_strarray.c
+++ ft_print_strarray.c
@@ -6,15 +6,15 @@
 /*   By: angkim <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/03/04 17:39:41 by angkim            #+#    #+#             */
-/*   Updated: 2019/03/08 16:27:39 by angkim           ###   ########.fr       */
+/*   Updated: 2019/03/16 08:44:03 by angkim           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
 /*
-** takes a string array and its length as arguments and prints each string
-** followed by a newline character to stdout
+** takes a string array and its size as arguments and displays each string
+** followed by a newline character
 */
 
 void	ft_print_strarray(char **array, int size)
